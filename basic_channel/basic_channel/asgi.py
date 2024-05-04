da2124a6ae371749077c3ee578d03c2435ad4fb3
H-M-Nizum <hmnizum1714032@gmail.com>
2024-05-05 01:14:07 +0600

    new something

--- basic_channel/basic_channel/asgi.py
+++ basic_channel/basic_channel/asgi.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
+import first_app.routing
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basic_channel.settings')
 
 application = ProtocolTypeRouter({
     'http' : get_asgi_application(),
- 
+    'websocket' : URLRouter(
+        first_app.routing.websocket_urlpatterns
+    )
 })
